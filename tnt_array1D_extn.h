bd07ed7cffcab41ec4c6fdf3ba6cad81574674af
Steven Dorsher <sdorsh1@lsu.edu>
2015-02-08 17:39:56 -0600

    Fixed paths for MY machine. Need some way to make them work for everyones

---
+++ tnt_array1D_extn.h
@@ -0,0 +1,129 @@
+#ifndef TNT_ARRAY1D_EXTN_H
+#define TNT_ARRAY1D_EXTN_H
+#include <cmath>
+
+
+#include "../tnt/tnt_array1d.h"
+//include "../tnt/tnt_array1d_utils.h"
+
+
+namespace TNT
+{
+
+  template <class T>
+    Array1D<T> sqrt(const Array1D<T> &A)
+    {
+      int n=A.dim();
+      Array1D<T> C(n);
+      for(int k=0; k<n; k++)
+	{
+	  C[k]=std::sqrt(A[k]);
+	}
+      return C;
+    }
+
+  template <class T>
+    Array1D<T> operator+(const Array1D<T> &A, const T B)
+    {
+      int n=A.dim();
+      Array1D<T> C(n);
+      for(int k=0; k<n; k++)
+	{
+	  C[k]=A[k]+B;
+	}
+      return C;
+    }
+  
+
+  template <class T>
+    Array1D<T> operator+(const T B, const Array1D<T> &A)
+    {
+      int n=A.dim();
+      Array1D<T> C(n);
+      for(int k=0; k<n; k++)
+	{
+	  C[k]=A[k]+B;
+	}
+      return C;
+    }
+
+
+  template <class T>
+    Array1D<T> operator*(const T B, const Array1D<T> &A)
+    {
+      int n=A.dim();
+      Array1D<T> C(n);
+      for(int k=0; k<n; k++)
+	{
+	  C[k]=A[k]*B;
+	}
+      return C;
+    }
+
+  template <class T>
+    Array1D<T> operator*(const Array1D<T> &A, const T B)
+    {
+      int n=A.dim();
+      Array1D<T> C(n);
+      for(int k=0; k<n; k++)
+	{
+	  C[k]=A[k]*B;
+	}
+      return C;
+    }
+
+  template <class T>
+    Array1D<T> operator/(const Array1D<T> &A, const T B)
+    {
+      int n=A.dim();
+      Array1D<T> C(n);
+      for(int k=0; k<n; k++)
+	{
+	  C[k]=A[k]/B;
+	}
+      return C;
+    }
+
+  template <class T>
+    Array1D<T> operator/(const T B, const Array1D<T> &A)
+    {
+      int n=A.dim();
+      Array1D<T> C(n);
+      for(int k=0; k<n; k++)
+	{
+	  C[k]=B/A[k];
+	}
+      return C;
+    }
+
+  template <class T>
+    Array1D<T> operator-(const T B, const Array1D<T> &A)
+    {
+      int n=A.dim();
+      Array1D<T> C(n);
+      for(int k=0; k<n; k++)
+	{
+	  C[k]=B-A[k];
+	}
+      return C;
+    }
+
+  template <class T>
+    Array1D<T> operator-(const Array1D<T> &A, const T B)
+    {
+      int n=A.dim();
+      Array1D<T> C(n);
+      for(int k=0; k<n; k++)
+	{
+	  C[k]=A[k]-B;
+	}
+      return C;
+    }
+
+  
+
+}
+
+
+
+#endif
---
+++ tnt_array2D_extn.h
@@ -0,0 +1,166 @@
+#ifndef TNT_ARRAY2D_EXTN_H
+#define TNT_ARRAY2D_EXTN_H
+#include <cmath>
+
+
+#include "../tnt/tnt_array2d.h"
+//include "../tnt/tnt_array2d_utils.h"
+
+
+namespace TNT
+{
+
+
+  template <class T>
+    Array2D<T> sqrt(const Array2D<T> &A)
+    {
+      int n=A.dim1();
+      int m=A.dim2();
+      Array2D<T> C(n,m);
+      for(int k=0; k<n; k++)
+	{
+	  for(int j=0; j<m; j++)
+	    {
+	      C[k][j]=std::sqrt(A[k][j]);
+	    }
+	}
+      return C;
+    }
+
+  template <class T>
+    Array2D<T> operator+(const Array2D<T> &A, const T B)
+    {
+      int n=A.dim1();
+      int m=A.dim2();
+      Array2D<T> C(n,m);
+      for(int k=0; k<n; k++)
+	{
+	  for(int j=0; j<m; j++)
+	    {
+	      C[k][j]=A[k][j]+B;
+	    }
+	}
+      return C;
+    }
+  
+
+  template <class T>
+    Array2D<T> operator+(const T B, const Array2D<T> &A)
+    {
+      int n=A.dim1();
+      int m=A.dim2();
+      Array2D<T> C(n,m);
+      for(int k=0; k<n; k++)
+	{
+	  for(int j=0; j<m; j++)
+	    {
+	      C[k][j]=A[k][j]+B;
+	    }
+	}
+      return C;
+    }
+
+
+  template <class T>
+    Array2D<T> operator*(const T B, const Array2D<T> &A)
+    {
+      int n=A.dim1();
+      int m=A.dim2();
+      Array2D<T> C(n,m);
+      for(int k=0; k<n; k++)
+	{
+	  for(int j=0; j<m; j++)
+	    {
+	      C[k][j]=A[k][j]*B;
+	    }
+	}
+      return C;
+    }
+
+  template <class T>
+    Array2D<T> operator*(const Array2D<T> &A, const T B)
+    {
+      int n=A.dim1();
+      int m=A.dim2();
+      Array2D<T> C(n,m);
+      for(int k=0; k<n; k++)
+	{
+	  for(int j=0;j<m;j++)
+	    {
+	      C[k][j]=A[k][j]*B;
+	    }
+	}
+      return C;
+    }
+
+  template <class T>
+    Array2D<T> operator/(const Array2D<T> &A, const T B)
+    {
+      int n=A.dim1();
+      int m=A.dim2();
+      Array2D<T> C(n,m);
+      for(int k=0; k<n; k++)
+	{
+	  for(int j=0; j<m; j++)
+	    {
+	      C[k][j]=A[k][j]/B;
+	    }
+	}
+      return C;
+    }
+
+  template <class T>
+    Array2D<T> operator/(const T B, const Array2D<T> &A)
+    {
+      int n=A.dim1();
+      int m=A.dim2();
+      Array2D<T> C(n,m);
+      for(int k=0; k<n; k++)
+	{
+	  for(int j=0; j<m; j++)
+	    {
+	      C[k][j]=B/A[k][j];
+	    }
+	}
+      return C;
+    }
+
+  template <class T>
+    Array2D<T> operator-(const T B, const Array2D<T> &A)
+    {
+      int n=A.dim1();
+      int m=A.dim2();
+      Array2D<T> C(n,m);
+      for(int k=0; k<n; k++)
+	{
+	  for(int j=0; j<m; j++)
+	    {
+	      C[k][j]=B-A[k][j];
+	    }
+	}
+      return C;
+    }
+
+  template <class T>
+    Array2D<T> operator-(const Array2D<T> &A, const T B)
+    {
+      int n=A.dim1();
+      int m=A.dim2();
+	Array2D<T> C(n,m);
+      for(int k=0; k<n; k++)
+	{
+	  for(int j=0; j<m; j++)
+	    {
+	      C[k][j]=A[k][j]-B;
+	    }
+	}
+      return C;
+    }
+
+  
+
+}
+
+
+
+#endif
