3248e710b2ae45581f46ff0a621371df3ee7419e
Frank Löffler <knarf@cct.lsu.edu>
2015-05-21 09:18:27 -0500

    some small changes, mostly cosmetics

--- Grid.cpp
+++ Grid.cpp
@@ -1,10 +1,8 @@
 #include "Grid.h"
 
-
-Grid::Grid(int elemorder, int numelements,double lowerlim, double upperlim):order{elemorder},NumElem{numelements},nodeLocs{0,elemorder+1,false}, refelem{elemorder}
+Grid::Grid(int elemorder, int numelements, double lowerlim, double upperlim) :
+           order{elemorder}, NumElem{numelements}, nodeLocs{0,elemorder+1,false}, refelem{elemorder}
 {
-  
-  
   for(int i=0; i<=numelements; i++)
     {
       elementBoundaries.push_back(lowerlim+i*(upperlim-lowerlim)/float(numelements));
@@ -13,23 +11,17 @@ Grid::Grid(int elemorder, int numelements,double lowerlim, double upperlim):orde
   Array1D<double> physicalPosition(elemorder+1);
   for(int elem=0; elem<numelements; elem++)
     {
-      physicalPosition=((elementBoundaries[elem+1]-elementBoundaries[elem])/2.0)
-        *refelem.getr()
-        +((elementBoundaries[elem+1]+elementBoundaries[elem])/2.0);
+      physicalPosition =  ((elementBoundaries[elem+1] - elementBoundaries[elem]) / 2.0)
+                          * refelem.getr()
+                        + ((elementBoundaries[elem+1] + elementBoundaries[elem]) / 2.0);
       nodeLocs.append(physicalPosition);
     }
-
   calcjacobian();
-  
-
 }
 
-
-
-Grid::Grid(string fileElemBoundaries,int elemorder, int numelements):order{elemorder},NumElem{numelements},nodeLocs{0,elemorder+1,false}, refelem{elemorder}
+Grid::Grid(string fileElemBoundaries, int elemorder, int numelements) : 
+          order{elemorder}, NumElem{numelements}, nodeLocs{0,elemorder+1,false}, refelem{elemorder}
 {
-  
-
   ifstream fs;
   fs.open(fileElemBoundaries);
   double data;
@@ -46,22 +38,18 @@ Grid::Grid(string fileElemBoundaries,int elemorder, int numelements):order{elemo
     }
 
   //  ReferenceElement refelem(elemord);
-  //when we generalize this, use map to store order, element pairs
-  //so they do not need to be recalculated with each element of the same
-  //order
-  
+  // when we generalize this, use map to store order, element pairs
+  // so they do not need to be recalculated with each element of the same
+  // order
   Array1D<double> physicalPosition(elemorder+1);
   for(int elem=0; elem<numelements; elem++)
     {
-      physicalPosition=((elementBoundaries[elem+1]-elementBoundaries[elem])/2.0)
-        *refelem.getr()
-        +((elementBoundaries[elem+1]+elementBoundaries[elem])/2.0);
+      physicalPosition = ((elementBoundaries[elem+1]-elementBoundaries[elem]) / 2.0)
+                          *refelem.getr()
+                       + ((elementBoundaries[elem+1]+elementBoundaries[elem]) / 2.0);
       nodeLocs.append(physicalPosition);
     }
-
   calcjacobian();
-  
-
 }
 
 int Grid::numberElements()
