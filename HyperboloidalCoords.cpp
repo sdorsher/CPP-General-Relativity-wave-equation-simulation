33165cf0d557d4d73e8b39e7fefee7630289f886
Steven Dorsher <sdorsh1@lsu.edu>
2015-07-06 21:07:18 -0500

    transition function now working

--- HyperboloidalCoords.cpp
+++ HyperboloidalCoords.cpp
@@ -72,3 +72,25 @@ double invert_tortoise(double rstar, double mass)
   }
   return invtort;
 }
+
+void transition(double rho, double R, double S, double& fT, double& fTp,
+                  double& fTpp) {
+  const double es = 1.5;
+  const double q2 = 1.3;
+  const double half = 0.5;
+  double x, tanx, cotx, fac, tanhfac, cscx2, secx2, sechfac2;
+
+  x = half * PI * (rho - R) / (S - R);
+  tanx = tan(x);
+  cotx = 1.0 / tanx;
+  fac = es / PI * (tanx - q2 * cotx);
+  tanhfac = tanh(fac);
+  fT = half + half * tanhfac;
+  cscx2 = 1.0 / pow(sin(x),2.0);
+  secx2 = 1.0 / pow(cos(x),2.0);
+  sechfac2 = 1.0 / pow(cosh(fac),2.0);
+  fTp = 0.25 * es * (q2 * cscx2 + secx2) * sechfac2 / (S - R);
+  fTpp = -0.25 * es * sechfac2 * 
+    (PI * (q2 * cotx * cscx2 - secx2 * tanx) 
+     + es * pow((q2 * cscx2 + secx2), 2.0) * tanhfac) / pow((S - R),2.);
+}
--- HyperboloidalCoords.h
+++ HyperboloidalCoords.h
@@ -1,3 +1,7 @@
+#ifndef HYPERBOLOIDALCOORDS_H
+#define HYPERBOLOIDALCOORDS_H
+
+#include "globals.h"
 #include <cmath>
 #include <iostream>
 
@@ -6,3 +10,7 @@ using namespace std;
 double Lambert(double z);
 double rschw(double z, double mass);
 double invert_tortoise(double rstar, double mass);
+void transition(double rho, double R, double S, double& fT, double& fTp,
+                double& fTpp);
+
+#endif
