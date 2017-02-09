b92935a8b9f6084d4083ec49fda64c985f724bde
Steven Dorsher <sdorsh1@lsu.edu>
2017-02-09 15:56:16 -0600

    reverting to before Omega experiment, where particle was read in different coordinate system, 5de6444

--- Grid.cpp
+++ Grid.cpp
@@ -110,7 +110,11 @@ void Grid::find_extract_radii(double rfinite, double rSplus, OutputIndices& ijou
     for(int node =0; node<=order; node++){
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
       if((fabs(rschw.get(elem,node)-rfinite)<1.0e-5) && (!foundfinite)) { 
+=======
+      if((fabs(nodeLocs.get(elem,node)-rfinite)<1.0e-5) && (!foundfinite)) { 
+>>>>>>> parent of 5de6444... fixed bug where location of particle was read out in different coordinate system than input. have not tested yet.
 =======
       if((fabs(nodeLocs.get(elem,node)-rfinite)<1.0e-5) && (!foundfinite)) { 
 >>>>>>> parent of 5de6444... fixed bug where location of particle was read out in different coordinate system than input. have not tested yet.
