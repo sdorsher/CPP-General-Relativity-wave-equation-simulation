59f75673d5f3a65cbd029afd0595ef25925af366
Steven Dorsher <sdorsh1@lsu.edu>
2017-02-09 15:42:57 -0600

    Fixed typo in thegrid.rschw

--- Grid.cpp
+++ Grid.cpp
@@ -102,7 +102,7 @@ int Grid::numberElements()
 
 void Grid::find_extract_radii(double rfinite, double rSplus, OutputIndices& ijoutput){
   //Find the grid and element indices that correspond the the computational
-  //coordinates rfinite and rSplus. rfinite in schwarzschild coordinates. splus in computational coordinates
+  //coordinates rfinite and rSplus.
   
   bool foundfinite = false;
   bool foundSplus = false;
